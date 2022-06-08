355fafc5e120fe8aa7ccf0348c722ea6790c3129
1719p <1719pankaj@gmail.com>
2022-06-08 13:45:28 +0530

    Inital commit

---
+++ Firebase.ino
@@ -0,0 +1,181 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+//Sensors ka include
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+
+//Sensors ka define
+#define DHTPIN 18 
+#define DHTTYPE DHT11   // DHT 11
+#define ONE_WIRE_BUS 2
+#define REPORTING_PERIOD_MS     1000
+
+// Insert your network credentials
+#define WIFI_SSID "J_WICK"
+#define WIFI_PASSWORD "bitemebitemebiteme"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyAEzIhYurT4SrWkRGug0JtGxjeu-Pop_nA"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "esp-demo-10faa-default-rtdb.asia-southeast1.firebasedatabase.app" 
+
+DHT dht(DHTPIN, DHTTYPE);
+OneWire oneWire(ONE_WIRE_BUS);  
+DallasTemperature sensors(&oneWire);
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+
+void onBeatDetected() {
+    Serial.println("Beat!");
+}
+
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println(F("DHT11 test!"));
+
+   Serial.print("Initializing pulse oximeter..");
+
+    // Initialize sensor
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+  // Configure sensor to use 7.6mA for LED drive
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback routine
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+    
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      // Read temperature as Fahrenheit (isFahrenheit = true)
+      float f = dht.readTemperature(true);
+
+      sensors.requestTemperatures(); 
+
+      pox.update();
+    
+    if (Firebase.RTDB.setInt(&fbdo, "test/BPM", pox.getHeartRate())){
+      Serial.println("BPM PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/SPO2", pox.getSpO2())){
+      Serial.println("SPO2 PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/HUMIDT", h)){
+      Serial.println("HUMIDT PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/TEMP_AMB_C", t)){
+      Serial.println("TAMP_AMB_C PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/HT_INDEX_C", dht.computeHeatIndex(t, h, false))){
+      Serial.println("HT_INDEX_C PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/HT_INDEX_F", dht.computeHeatIndex(f, h))){
+      Serial.println("HT_INDEX_F PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, "test/TEMP_CONT_C", sensors.getTempCByIndex(0))){
+      Serial.println("TEMP_CONT_C PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
